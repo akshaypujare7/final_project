7ed90f7dcadcd8ab428d0e0231c43683aa8660c4
akshaypujare <akshaypujare7@gmail.com>
2022-05-10 15:17:02 -0400

    added simple pages

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -1,3 +1,5 @@
+import os.path
+
 import click
 from flask.cli import with_appcontext
 from app.db import db
@@ -6,4 +8,10 @@ from app.db import db
 @click.command(name='create-db')
 @with_appcontext
 def create_database():
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    db_dir = os.path.join(file_dir, "../../database")
+    print(db_dir)
+
+    if not os.path.exists(db_dir):
+        os.mkdir(db_dir)
     db.create_all()
