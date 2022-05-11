35902a700a1fe1c1db0429e38b0208ca5c8d2615
akshaypujare <akshaypujare7@gmail.com>
2022-05-11 09:27:05 -0400

    added config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,68 @@
 """A simple flask web app"""
 import os
-from flask import Flask, render_template
+from flask import Flask
+from flask import Blueprint, render_template, abort
+from flask_wtf.csrf import CSRFProtect
+
+import flask_login
+from flask_bootstrap import Bootstrap5
+
 from app.cli import create_database
 from app.db import db
+from app.simple_pages import simple_pages
+from app.auth import auth
 from app.db.models import User
+from app.context_processors import utility_text_processors
+
+login_manager = flask_login.LoginManager()
 
 
 def page_not_found(e):
     return render_template("404.html"), 404
 
+
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif app.config["ENV"] == "testing":
+        app.config.from_object("app.config.TestingConfig")
+
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap5(app)
 
     app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+
+    app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
+    #  app.register_blueprint(transactions)
+    #  app.register_blueprint(log_con)
+
+    app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
 
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    # db_dir = "database/db.sqlite"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
+    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    # @app.route('/')
+    # def hello():
+    #     return 'Hello, World!'
 
     return app
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
---
+++ app/config.py
@@ -0,0 +1,31 @@
+import os
+
+
+class Config(object):
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    SESSION_COOKIE_SECURE = True
+    BOOTSTRAP_BOOTSWATCH_THEME = 'Simplex'
+    db_dir = "database/db.sqlite"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.abspath(db_dir)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    WTF_CSRF_ENABLED = False
+    SESSION_COOKIE_SECURE = False
+    DEBUG = True
